e72edb6318e653c28d188f3a8ac369daf3d10701
R Aadarsh <raadarsh1107@outlook.com>
2025-10-05 18:58:06 +0530

    - Add optional encoding parameter to Worktree::load_file
    - Remove the parameter from `BufferStore::open_buffer` as it is not
    needed

--- crates/worktree/src/worktree_tests.rs
+++ crates/worktree/src/worktree_tests.rs
@@ -466,7 +466,7 @@ async fn test_open_gitignored_files(cx: &mut TestAppContext) {
     let prev_read_dir_count = fs.read_dir_call_count();
     let loaded = tree
         .update(cx, |tree, cx| {
-            tree.load_file(rel_path("one/node_modules/b/b1.js"), cx)
+            tree.load_file("one/node_modules/b/b1.js".as_ref(), None, cx)
         })
         .await
         .unwrap();
@@ -506,7 +506,7 @@ async fn test_open_gitignored_files(cx: &mut TestAppContext) {
     let prev_read_dir_count = fs.read_dir_call_count();
     let loaded = tree
         .update(cx, |tree, cx| {
-            tree.load_file(rel_path("one/node_modules/a/a2.js"), cx)
+            tree.load_file("one/node_modules/a/a2.js".as_ref(), None, cx)
         })
         .await
         .unwrap();
