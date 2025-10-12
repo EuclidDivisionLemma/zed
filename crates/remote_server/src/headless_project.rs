a5bc1fc994f8547e9c36f3f1b1769ee528e5776f
R Aadarsh <raadarsh1107@outlook.com>
2025-10-12 16:45:30 +0530

    Enable a file to be opened with an invalid encoding with the invalid bytes replaced with replacement characters
    - Fix UTF-16 file handling
    
    - Introduce a `ForceOpen` action to allow users to open files despite
    encoding errors
    
    - Add `force` and `detect_utf16` flags
    
    - Update UI to provide "Accept the Risk and Open" button for invalid
    encoding files

--- crates/remote_server/src/headless_project.rs
+++ crates/remote_server/src/headless_project.rs
@@ -517,6 +517,8 @@ impl HeadlessProject {
                         path: Arc::<Path>::from_proto(message.payload.path),
                     },
                     None,
+                    false,
+                    true,
                     cx,
                 )
             });
@@ -677,6 +679,8 @@ impl HeadlessProject {
                         path: path,
                     },
                     None,
+                    false,
+                    true,
                     cx,
                 )
             });
